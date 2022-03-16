f60966308c0bb22f0d21e525fa8d607deb18a67a
nimrod <nimicu21@gmail.com>
2022-03-16 09:40:25 +0200

    config update

--- Lab2/lab02_student.py
+++ Lab2/lab02_student.py
@@ -1,12 +1,16 @@
 import numpy as np
 from sympy import *
 from scipy.spatial.transform import Rotation as R
+import yaml
 
 alpha1, alpha2, alpha3, alpha4, alpha5, alpha6 = symbols('alpha1:7')
 a1, a2, a3, a4, a5, a6 = symbols('a1:7')
 d1, d2, d3, d4, d5, d6 = symbols('d1:7')
 q1, q2, q3, q4, q5, q6 = symbols('q1:7')
 
+stream = open("config_kinovaGen3.yaml",'r')
+kinova = yaml.load(stream)
+print(kinova)
 
 def set_dh_table():
     """
