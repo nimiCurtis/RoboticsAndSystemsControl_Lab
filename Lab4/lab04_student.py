9fb7d96d6ad4c02d34681a38f13e681ead575ec8
nimiCurtis <nimicu21@gmail.com>
2022-04-05 14:39:39 +0300

    update comments

--- Lab4/lab04_student.py
+++ Lab4/lab04_student.py
@@ -24,16 +24,16 @@ def traj_gen_config(q1, q2, t, Tf):
     ## according to start&end pos,vel,accel we can calc a0 - a5
     ## and then we can find q'(t) , q''(t) of a given t
     
-    # define w1 = start vel & w2 = end vel
-    w1 = np.array([0 , 0 , 0 , 0 , 0 , 0 ])
+    # define w1 = start vel & w2 = end vel [deg/s]
+    w1 = np.array([0 , 0 , 0 , 0 , 0 , 0 ]) 
     w2 = np.array([0 , 0 , 0 , 0 , 0 , 0 ])
 
-    # define alpha1 = start accel & alpha2 = end accel
+    # define alpha1 = start accel & alpha2 = end accel [deg/(s^2)]
     alpha1 = np.array([0 , 0 , 0 , 0 , 0 , 0 ])
     alpha2 = np.array([0 , 0 , 0 , 0 , 0 , 0 ])
 
     # based on the algebric equation X = Aa
-    # set results 'X' vector  --> np.array((6,6))
+    # set results 'X' vector  --> np.array((6,6)) 
     X  = np.array([q1,w1,alpha1,q2,w2,alpha2])
 
     # set coefficients 'A' matrix --> np.array((6,6))
@@ -49,7 +49,7 @@ def traj_gen_config(q1, q2, t, Tf):
     # calc coefficients 'a' vector by a = inv(A)*X --> np.array((6,6))
     a = np.matmul(np.linalg.inv(A),X)
 
-    # calc q,dq,ddq by the quintic polynomial equation and the derivatives of q(t) >> wrote above
+    # calc q,dq,ddq by the quintic polynomial equation and the derivatives of q(t) >> writen in the first comment 
     q   = np.dot( np.array([1,t,t**2,t**3,t**4,t**5]),a ) # equals to a[0]*1 + a[1]*t + a[2]*t^2 + ...  --> np.array((6,)) [deg]
     dq  = np.dot( np.array([0,1,2*t,3*(t**2),4*(t**3),5*(t**4)]), a) # --> np.array((6,)) [deg/s]
     ddq = np.dot( np.array([0,0,2,6*t,12*(t**2),20*(t**3)]) ,a ) # --> np.array((6,)) [deg/(s^2)]
